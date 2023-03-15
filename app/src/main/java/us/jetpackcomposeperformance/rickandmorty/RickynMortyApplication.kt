3920dd401318d9775ff187b673e92a6fc7c93225
Beatrice Kinya <gitongakinya1@gmail.com>
2023-03-15 15:44:53 +0300

    add hilt

--- app/src/main/java/us/jetpackcomposeperformance/rickandmorty/RickynMortyApplication.kt
+++ app/src/main/java/us/jetpackcomposeperformance/rickandmorty/RickynMortyApplication.kt
@@ -1,9 +1,11 @@
 package us.jetpackcomposeperformance.rickandmorty
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import logcat.AndroidLogcatLogger
 import logcat.LogPriority
 
+@HiltAndroidApp
 class RickynMortyApplication: Application() {
 
   override fun onCreate() {
