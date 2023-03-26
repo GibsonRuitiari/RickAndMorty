56dbe96f099b1ed86ba6e44042d937d26a4f0772
Beatrice Kinya <gitongakinya1@gmail.com>
2023-03-26 16:32:43 +0300

    send the initial event

--- app/src/main/java/us/jetpackcomposeperformance/rickandmorty/ui/screens/RickAndMortyViewModel.kt
+++ app/src/main/java/us/jetpackcomposeperformance/rickandmorty/ui/screens/RickAndMortyViewModel.kt
@@ -4,7 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import us.jetpackcomposeperformance.rickandmorty.domain.models.CharacterResult
 import us.jetpackcomposeperformance.rickandmorty.domain.repository.CharacterRepository
@@ -12,53 +16,56 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RickAndMortyViewModel @Inject constructor(private val characterRepository: CharacterRepository) :
-  ViewModel() {
-  private val uiEvents = MutableSharedFlow<RickAndMortyEvents>(extraBufferCapacity = 10)
-  private val uiState = MutableStateFlow<RickAndMortyState>(RickAndMortyState.Loading)
+    ViewModel() {
+    private val uiEvents = MutableSharedFlow<RickAndMortyEvents>(extraBufferCapacity = 10)
+    private val uiState = MutableStateFlow<RickAndMortyState>(RickAndMortyState.Loading)
 
-  val rickAndMortyState: StateFlow<RickAndMortyState>
-    get() = uiState
+    val rickAndMortyState: StateFlow<RickAndMortyState>
+        get() = uiState
 
-  init {
-    observeEvents()
-  }
+    init {
+        observeEvents()
+    }
 
-  private fun loadCharactersFromApi() {
-    viewModelScope.launch {
-      characterRepository.getCharacters()
-        .onStart {
-          uiState.value = RickAndMortyState.Loading
-        }
-        .catch { ex ->
-          if (ex !is CancellationException) {
-            uiState.value = RickAndMortyState.Error(ex.message ?: "an unknown error occurred")
-          }
-        }.collect {
-          when (it) {
-            is CharacterResult.Data -> uiState.value = RickAndMortyState.Data(it.data)
-            is CharacterResult.Empty -> uiState.value = RickAndMortyState.Data.DefaultState
-            is CharacterResult.Error -> uiState.value = RickAndMortyState.Error(it.message)
-          }
+    private fun loadCharactersFromApi() {
+        viewModelScope.launch {
+            characterRepository.getCharacters()
+                .onStart {
+                    uiState.value = RickAndMortyState.Loading
+                }
+                .catch { ex ->
+                    if (ex !is CancellationException) {
+                        uiState.value =
+                            RickAndMortyState.Error(ex.message ?: "an unknown error occurred")
+                    }
+                }.collect {
+                    when (it) {
+                        is CharacterResult.Data -> uiState.value = RickAndMortyState.Data(it.data)
+                        is CharacterResult.Empty -> uiState.value =
+                            RickAndMortyState.Data.DefaultState
+                        is CharacterResult.Error -> uiState.value =
+                            RickAndMortyState.Error(it.message)
+                    }
+                }
         }
     }
-  }
 
-  fun sendEvent(uiEvent: RickAndMortyEvents) {
-    uiEvents.tryEmit(uiEvent)
-  }
-
-  private fun observeEvents() {
-    viewModelScope.launch {
-      uiEvents.collect {
-        when (it) {
-          RickAndMortyEvents.LoadCharacters -> {
-            loadCharactersFromApi()
-          }
-          RickAndMortyEvents.RetryLoadingCharacters -> {
-            sendEvent(RickAndMortyEvents.LoadCharacters)
-          }
-        }
-      }
+    fun sendEvent(uiEvent: RickAndMortyEvents) {
+        uiEvents.tryEmit(uiEvent)
     }
-  }
+
+    private fun observeEvents() {
+        viewModelScope.launch {
+            uiEvents.collect {
+                when (it) {
+                    RickAndMortyEvents.LoadCharacters -> {
+                        loadCharactersFromApi()
+                    }
+                    RickAndMortyEvents.RetryLoadingCharacters -> {
+                        sendEvent(RickAndMortyEvents.LoadCharacters)
+                    }
+                }
+            }
+        }
+    }
 }
